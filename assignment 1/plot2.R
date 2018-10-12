10d82bd018f89759304251658e03fefc59c243d6
Laurens de Wit <31757335+laurensdw@users.noreply.github.com>
2018-10-12 13:16:39 +0200

    Added code for second plot

--- assignment 1/plot2.R
+++ assignment 1/plot2.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(lubridate)
 
 setwd("C:/Users/ldewit/Documents/coursera_local/Exploratory_Data_Analysis_Coursera/assignment 1")
 
@@ -9,5 +10,13 @@ df <- read.csv("../../Data/household_power_consumption.txt", header=T, sep=';', 
 ## Manipulating and subsetting data
 df$Date <- as.Date(df$Date, format="%d/%m/%Y")
 df <- filter(df, Date == as.Date('2007-02-01') | Date == as.Date('2007-02-02'))
+datetime <- paste(as.Date(df$Date), df$Time)
+df$Datetime <- as.POSIXct(datetime)
 
 ## Plot2
+plot(df$Global_active_power~df$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+
+## Saving to file
+dev.copy(png, file="plot2.png", height=500, width=500)
+dev.off()
